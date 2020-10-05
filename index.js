462643b8a391c6e71899c511ba62594aeab1dee4
souemon16 <souemon16@gmail.com>
2020-10-06 03:18:57 +0600

    filtered query fix and dlt button not fixed

--- index.js
+++ index.js
@@ -15,58 +15,77 @@ app.use(cors())
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const eventsCollection = client.db("volunteerNetwork").collection("events");
-  console.log("event database conected")
+    const eventsCollection = client.db("volunteerNetwork").collection("events");
+    console.log("event database conected")
 
-  // Get Datas from DB 
-  app.get('/events', (req, res) => {
-    eventsCollection.find({}).limit(20)
-    .toArray((err, documents) => {
-      res.send(documents);
+    // Get Datas from DB 
+    app.get('/events', (req, res) => {
+        eventsCollection.find({}).limit(20)
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
-  })
 
-  // Add Events To DB 
-  app.post('/addEvent', (req, res) => {
-    const newEvent = req.body;
-    eventsCollection.insertOne(newEvent)
-    .then(result.insertedCount > 0);
-  })
+    // Add Events To DB 
+    app.post('/addEvent', (req, res) => {
+        const newEvent = req.body;
+        eventsCollection.insertOne(newEvent)
+            .then(result.insertedCount > 0);
+    })
 });
 
 client.connect(err => {
-  const volunteerList = client.db("volunteerNetwork").collection("volunteerList");
-  console.log("volunteerList database conected")
+    const volunteerList = client.db("volunteerNetwork").collection("volunteerList");
+    console.log("volunteerList database conected")
 
-  // Save Data into DB 
-  app.post('/addVolunteer', (req, res) => {
-    const newVolunteer = req.body;
-    volunteerList.insertOne(newVolunteer)
-    .then(result => {
-      res.send(result.insertedCount > 0);
-      console.log(result);
+    // Save Data into DB 
+    app.post('/addVolunteer', (req, res) => {
+        const newVolunteer = req.body;
+        volunteerList.insertOne(newVolunteer)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+                console.log(result);
+            })
     })
-  })
 
-  // Get Data from DB 
-  app.get('/volunteerList', (req, res) => {
-    volunteerList.find({email: req.query.userEmail})
-    .toArray((err, documents) => { 
-      res.send(documents);
-    })
-  })
 
-  app.delete('/delete/:id', (req, res) => {
-    volunteerList.deleteOne({_id: ObjectId(req.params._id)})
-    .then((err, result) => {
-      console.log(result)
+    // Get Filtered Data from DB 
+    app.get('/filteredVolunteerList', (req, res) => {
+        volunteerList.find({ volunteerEmail: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
-  })
+
+    // Get Data from DB 
+    app.get('/volunteerList', (req, res) => {
+        volunteerList.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.delete('/deleteUser/:id', (req, res) => {
+        console.log(req.params.id);
+        volunteerList.deleteOne({ _id: objectId(req.params.id) })
+        .then((result) => {
+          res.send(result.deletedCount > 0);
+        });
+    })
+
+
+    app.delete('/delete/:id', (req, res) => {
+        console.log(req.params.id)
+        volunteerList.deleteOne({ id: ObjectId(req.params.id) })
+            .then((err, result) => {
+                console.log(result)
+            })
+    })
 });
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(process.env.PORT || port)
